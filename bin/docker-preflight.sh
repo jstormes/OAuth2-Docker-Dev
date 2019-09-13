a80c610bc42bb73693ada1096cc0a1fc5d74381c
James Stormes <james.s@digitalroominc.com>
2019-09-13 11:25:32 -0500

    XDebug Fixed.

--- bin/docker-preflight.sh
+++ bin/docker-preflight.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+SCRIPT=$(readlink -f "$0")
+
+echo "############################################################################################################"
+echo "# Starting $SCRIPT"
+echo "############################################################################################################"
+
 apt-get update -y &> /dev/null
 
 ############################################################################
@@ -9,6 +15,7 @@ apt-get update -y &> /dev/null
 ############################################################################
 if ! which composer &> /dev/null
 then
+    echo "Installing Composer"
     apt-get install -y wget git
     wget https://getcomposer.org/installer
     php installer
@@ -21,22 +28,25 @@ fi
 # Setup XDebug, always try and start XDebug connection to requesting ip
 # DO NOT DO THIS ON PUBLICLY ACCESSIBLE SYSTEMS!!!!!!!
 ############################################################################
-if ! find /usr/local/lib/php/extensions/ -name xdebug.so &> /dev/null
+if [[ ! $(find /usr/local/lib/php/extensions/ -name xdebug.so &> /dev/null) ]];
 then
+    echo "Installing XDEBUG"
     yes | pecl install xdebug
     echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini
     echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini
     echo "xdebug.remote_autostart=on" >> /usr/local/etc/php/conf.d/xdebug.ini
     echo "xdebug.remote_connect_back=on"  >> /usr/local/etc/php/conf.d/xdebug.ini
     echo "xdebug.idekey=default-docker" >> /usr/local/etc/php/conf.d/xdebug.ini
+    echo "xdebug.remote_host=host.docker.internal" >> /usr/local/etc/php/conf.d/xdebug.ini
 fi
 
 ############################################################################
 # set host.docker.internal if it is not set
 ############################################################################
 apt-get install -y inetutils-ping iproute2 &> /dev/null
-if ! ping -c 1 host.docker.internal &> /dev/null
+if [[ ! $(ping -c 1 host.docker.internal &> /dev/null) ]];
 then
+  echo "Adding host host.docker.internal\n\n"
   ip -4 route list match 0/0 | awk '{print $3 " host.docker.internal"}' >> /etc/hosts
 fi
 
@@ -51,7 +61,12 @@ chmod a+x /root/bin/*
 ############################################################################
 if [[ ! -d "/var/www/vendor" ]]
 then
+    echo "Installing Composer packages"
     cd /var/www
     composer install
     composer development-enable
 fi
+
+echo "############################################################################################################"
+echo "# Exit $SCRIPT"
+echo "############################################################################################################"
